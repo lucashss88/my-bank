111839bb5998e025a7c3c55964256e4ac0a91119
cbarbosagomesfilho <cbarbosagomesfilho@gmail.com>
2025-06-07 15:55:07 -0300

    create my-bank: root project

---
+++ ms-customer/src/main/java/br/com/bank/ApplicationCustomer.java
@@ -0,0 +1,11 @@
+package br.com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCustomer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationCustomer.class, args);
+    }
+}
